8aec213e0a483cb440caae6bd9e432ddd93e7f05
deluxesande <deluxesande@gmail.com>
2024-07-22 03:33:58 +0300

    code clean up

--- pages/api/product/get.ts
+++ pages/api/product/get.ts
@@ -3,10 +3,8 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const getProducts = async (
-    req: NextApiRequest,
-    res: NextApiResponse
-) => {
+export const getProducts = async (req: NextApiRequest, res: NextApiResponse) => {
     const products = await prisma.product.findMany();
     res.status(200).json(products);
 };
+
