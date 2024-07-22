0bc3ea17b587ae9778a6611feb0af9e859f71dfd
deluxesande <deluxesande@gmail.com>
2024-07-22 03:02:49 +0300

    feat: successful connection to supabase db with prisma

--- pages/api/product/getproducts.ts
+++ pages/api/product/getproducts.ts
@@ -1,6 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { products } from "@/pages/utils/products";
+import { PrismaClient } from "@prisma/client";
 
-export const getProducts = (req: NextApiRequest, res: NextApiResponse) => {
+const prisma = new PrismaClient();
+
+export const getProducts = async (
+    req: NextApiRequest,
+    res: NextApiResponse
+) => {
+    const products = await prisma.product.findMany();
     res.status(200).json(products);
 };
