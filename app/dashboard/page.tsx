ce3769853f713381be2e93eca0f9370dc8963054
deluxesande <deluxesande@gmail.com>
2024-09-16 00:29:52 +0300

    Complete top bar navigation

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@components/Navbar";
 
 export default function index() {
     return (
-        <div className="w-full h-full bg-red-500">
+        <div>
             <Navbar />
         </div>
     );
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,27 +1,38 @@
-import { Bell } from "lucide-react";
+import { Bell, SlidersHorizontal, Search as SearchIcon } from "lucide-react";
 import React from "react";
 
 export default function Navbar() {
     return (
-        <div className="p-6 bg-red-200">
-            <div className="flex items-center  justify-between">
-                <h1 className="text-2xl">LOGO</h1>
-                <ul className="flex">
-                    <li>
-                        <a href="/" className="text-lg">
-                            Home
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/dashboard" className="text-lg ml-4">
-                            Dashboard
-                        </a>
-                    </li>
-
-                    <div className="p-2 bg-slate-50 rounded-lg">
-                        <Bell />
+        <div className="p-6">
+            <div className="flex items-center  justify-between gap-4">
+                <div className="flex items-center w-full">
+                    <div className="flex items-center mx-4 bg-white rounded-lg shadow-sm flex-grow">
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                            className="py-3 px-4 rounded-l-lg focus:outline-none w-full"
+                        />
+                        <div className="py-2 px-4">
+                            <SearchIcon size={25} />
+                        </div>
                     </div>
-                </ul>
+                    <div className="p-2 bg-white rounded-lg cursor-pointer flex items-center justify-center">
+                        <SlidersHorizontal size={30} />
+                    </div>
+                </div>
+
+                <div className="flex items-center">
+                    <div className="p-2 bg-white rounded-lg flex items-center gap-4 cursor-pointer">
+                        <div className="w-8 h-8 rounded-full bg-red-500"></div>
+                        <div className="w-28">
+                            <p className="font-bold text-sm">John Doe</p>
+                            <p className="font-light text-xs">Admin</p>
+                        </div>
+                    </div>
+                    <div className="p-2 bg-white mx-4 rounded-lg cursor-pointer flex items-center justify-center">
+                        <Bell size={30} />
+                    </div>
+                </div>
             </div>
         </div>
     );
