54c3d2d11d5bc4fece02b7248eb9de56050b271f
deluxesande <deluxesande@gmail.com>
2025-01-04 12:20:13 +0300

    Failed integration of mongo replica set, key not fetching

--- pages/api/category/index.ts
+++ pages/api/category/index.ts
@@ -4,11 +4,11 @@ import { addCategory } from "./post";
 import { getAuth } from "@clerk/nextjs/server";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { userId } = getAuth(req);
+    // const { userId } = getAuth(req);
 
-    if (!userId) {
-        return res.status(401).json({ error: "Unauthorized" });
-    }
+    // if (!userId) {
+    //     return res.status(401).json({ error: "Unauthorized" });
+    // }
 
     switch (req.method) {
         case "GET":
