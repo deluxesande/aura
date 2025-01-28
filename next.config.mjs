cee4ae49b67efb031e8ec6f0df9eb8b1ccf1cca3
deluxesande <deluxesande@gmail.com>
2025-01-28 16:20:58 +0300

    missingSuspenseWithCSRBailout

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
     images: {
         dangerouslyAllowSVG: true,
+        experimental: {
+            missingSuspenseWithCSRBailout: false,
+        },
         remotePatterns: [
             {
                 protocol: "https",
