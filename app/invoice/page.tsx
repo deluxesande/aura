30ee0c10016e297444e82ca2946a4203aee3645f
deluxesande <deluxesande@gmail.com>
2025-12-28 20:45:21 +0300

    changed default product image from jpg to png

--- app/invoice/page.tsx
+++ app/invoice/page.tsx
@@ -188,9 +188,6 @@ function InvoicePageContent() {
                             <Printer size={16} />
                             Print
                         </button>
-                        <button className="flex items-center gap-2 px-4 py-2 bg-white border border-gray-200 rounded-lg text-sm font-medium text-gray-700 hover:bg-gray-50 transition-colors">
-                            Download
-                        </button>
                     </div>
                 </div>
 
