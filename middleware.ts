da173ac0dce9db59c7c221cca5e6b5cc431b8bf0
deluxesande <deluxesande@gmail.com>
2026-03-17 18:29:12 +0300

    added api to protected routes

--- middleware.ts
+++ middleware.ts
@@ -9,6 +9,7 @@ const isPrivateRoute = createRouteMatcher([
     "/customer(.*)",
     "/profile(.*)",
     "/settings(.*)",
+    "/api/(.*)",
 ]);
 
 // const isPrivateRoute = createRouteMatcher([]);
